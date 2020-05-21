a2635f877764cce7251fad05fd5e11a516f9045e
Jaroslaw Herod <jaroslaw.herod@gmail.com>
2020-05-21 22:40:02 +0200

    Initial commit of FizzBuzz Kata

---
+++ src/test/java/io/c8y/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package io.c8y;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void goodPlaceToStart(){
+        assertTrue( true );
+    }
+
+}
